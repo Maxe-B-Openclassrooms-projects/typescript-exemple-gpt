04c9083ee84aec6e1a4aab265762ac02f729db8d
MaxeBar <maxence.baranger.mb@gmail.com>
2023-10-26 19:14:13 +0200

    juste un peu espace entre les lignes pour une meilleure visibilité

--- dist/index.js
+++ dist/index.js
@@ -3,15 +3,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+
+
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
+
 const app = (0, express_1.default)();
 const port = 3000;
+
 app.use(body_parser_1.default.json());
+
 let tasks = [];
+
 app.get('/tasks', (req, res) => {
     res.json(tasks);
 });
+
 app.post('/tasks', (req, res) => {
     const task = {
         id: tasks.length + 1,
@@ -21,6 +28,7 @@ app.post('/tasks', (req, res) => {
     tasks.push(task);
     res.json(task);
 });
+
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
 });
